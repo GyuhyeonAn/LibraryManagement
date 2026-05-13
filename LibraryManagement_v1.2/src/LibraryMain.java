622ccf411b9727dd9f936f723ff0ff2c0616b25b
GyuhyeonAn <Gyuhyeon0035@naver.com>
2026-05-13 11:13:48 +0900

    Revert "날짜 추가"
    This reverts commit 7d5b89c9a0001ec530e358bb9e53c8f36f032044.

--- LibraryManagement_v1.2/src/LibraryMain.java
+++ LibraryManagement_v1.2/src/LibraryMain.java
@@ -4,9 +4,6 @@ public class LibraryMain {
     private static LibraryManager manager;
     private static Scanner sc = new Scanner(System.in);
 
-    /**
-    * <p>26.05.13 시작</p>
-    **/
     public static void main(String[] args) {
         LibraryRepository repo = new LibraryRepository();
         manager = new LibraryManager(repo);

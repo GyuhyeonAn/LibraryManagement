7d5b89c9a0001ec530e358bb9e53c8f36f032044
GyuhyeonAn <Gyuhyeon0035@naver.com>
2026-05-13 11:11:28 +0900

    날짜 추가

--- LibraryManagement_v1.2/src/LibraryMain.java
+++ LibraryManagement_v1.2/src/LibraryMain.java
@@ -4,6 +4,9 @@ public class LibraryMain {
     private static LibraryManager manager;
     private static Scanner sc = new Scanner(System.in);
 
+    /**
+    * <p>26.05.13 시작</p>
+    **/
     public static void main(String[] args) {
         LibraryRepository repo = new LibraryRepository();
         manager = new LibraryManager(repo);

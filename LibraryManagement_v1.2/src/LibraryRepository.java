05e54e57e5a2286fa3b3e74d233079ff0612f827
GyuhyeonAn <Gyuhyeon0035@naver.com>
2026-06-09 14:11:06 +0900

    코드 수정

--- LibraryManagement_v1.2/src/LibraryRepository.java
+++ LibraryManagement_v1.2/src/LibraryRepository.java
@@ -144,8 +144,8 @@ public class LibraryRepository {
      * @see <a href="https://github.com/sumannam/Java/issues/40">Issue #40: SQL Injection 취약점 개발</a>
      */
     public User loadUser(String id, String pw) {
-        //String sql = "SELECT * FROM users WHERE user_id = ? AND password = ?";
-        String sql = "SELECT * FROM users WHERE user_id = '" + id + "' AND password = '" + pw + "'";
+        String sql = "SELECT * FROM users WHERE user_id = ? AND password = ?";
+        //String sql = "SELECT * FROM users WHERE user_id = '" + id + "' AND password = '" + pw + "'";
         System.out.println(sql);
 
         if (!id.matches("^[a-zA-Z0-9]{4,15}$")) {
